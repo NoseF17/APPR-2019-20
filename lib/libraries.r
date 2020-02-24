65649378956d0adfc180d03ef2149b9641e8ccf6
NoseF17 <filip.nose@gmail.com>
2020-02-24 14:48:36 +0100

    Uredil knjižnice

--- lib/libraries.r
+++ lib/libraries.r
@@ -3,6 +3,17 @@ library(rvest)
 library(gsubfn)
 library(tidyr)
 library(shiny)
+library(readr)
+library(dplyr)
+library(mosaic)
+library(farver)
+
+options(gsubfn.engine="R")
+library("readxl")
+library("openxlsx")
+library(tmap)
+library(ggplot2)
+
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
