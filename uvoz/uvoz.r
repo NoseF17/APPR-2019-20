33f19fd0567db1983e42197438bea64bd55078de
Filip Nose <filip.nose@student.fmf.uni-lj.si>
2020-03-06 15:40:39 +0100

    Urejanje grafa Slo in EU skupaj. Popravek "na" na pravilne manjkajoče vrednosti

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -11,14 +11,14 @@ sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
 #tabela111 <- read_csv('podatki/lfsa_ewhun2_1_Data.csv', locale= locale(encoding = 'CP1250'))
 
-tabela1_delovne_ure_moski_zenske <- read_csv('podatki/tabela_moski_zenske_2008+.csv', na=' ', skip=1, col_names = c('Leto','Drzava','Spol','Zaposlenost','izbriši1','izbriši2','izbriši3', 'St. delovnih ur', 'izbriši4'), locale= locale(encoding = 'CP1250')) %>%
+tabela1_delovne_ure_moski_zenske <- read_csv('podatki/tabela_moski_zenske_2008+.csv', na=':', skip=1, col_names = c('Leto','Drzava','Spol','Zaposlenost','izbriši1','izbriši2','izbriši3', 'St. delovnih ur', 'izbriši4'), locale= locale(encoding = 'CP1250')) %>%
   select(-'izbriši1',-'izbriši2',-'izbriši3',-'izbriši4')
-tabela2panoge <- read_csv('podatki/tabela_po_panogah_2008+.csv', na=' ', skip=1, col_names = c('Leto','Drzava','Spol','Zaposlenost', 'Status', 'Panoga', 'Enota', 'St. delovnih ur', 'izbriši1'), locale= locale(encoding = 'CP1250')) %>%
+tabela2panoge <- read_csv('podatki/tabela_po_panogah_2008+.csv', na=':', skip=1, col_names = c('Leto','Drzava','Spol','Zaposlenost', 'Status', 'Panoga', 'Enota', 'St. delovnih ur', 'izbriši1'), locale= locale(encoding = 'CP1250')) %>%
   select(-'Spol',-'Status',-'izbriši1',-'Enota')
-tabela3gdp <- read_csv('podatki/tabela_GDP_per_capita_2008+.csv', na=' ', skip=1, col_names = c('Leto', 'Drzava', 'Enota','NA_ITEM', 'Vrednost', 'izbriši1'), locale= locale(encoding = 'CP1250')) %>%
+tabela3gdp <- read_csv('podatki/tabela_GDP_per_capita_2008+.csv', na=':', skip=1, col_names = c('Leto', 'Drzava', 'Enota','NA_ITEM', 'Vrednost', 'izbriši1'), locale= locale(encoding = 'CP1250')) %>%
   select(-'izbriši1')
 
-tabela4_delovne_ure_moski_zenske <- read_csv('podatki/tabela_moski_zenske_2008+.csv', na=' ', skip=1, col_names = c('Leto','Drzava','Spol','Zaposlenost','izbriši1','izbriši2','izbriši3', 'SteviloDelovnihUr', 'izbriši4'), locale= locale(encoding = 'CP1250')) %>%
+tabela4_delovne_ure_moski_zenske <- read_csv('podatki/tabela_moski_zenske_2008+.csv', na=':', skip=1, col_names = c('Leto','Drzava','Spol','Zaposlenost','izbriši1','izbriši2','izbriši3', 'SteviloDelovnihUr', 'izbriši4'), locale= locale(encoding = 'CP1250')) %>%
   select(-'izbriši1',-'izbriši2',-'izbriši3',-'izbriši4')
 
 
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,8 +1,8 @@
 # 3. faza: Vizualizacija podatkov
 
-A3$SteviloDelovnihUr <- parse_double(A3$SteviloDelovnihUr)
-A2$SteviloDelovnihUr <- parse_double(A2$SteviloDelovnihUr)
-A1$SteviloDelovnihUr <- parse_double(A1$SteviloDelovnihUr)
+#A3$SteviloDelovnihUr <- parse_double(A3$SteviloDelovnihUr)
+#A2$SteviloDelovnihUr <- parse_double(A2$SteviloDelovnihUr)
+#A1$SteviloDelovnihUr <- parse_double(A1$SteviloDelovnihUr)
 
 
 g1slo <- ggplot(data = A2, aes(x=Leto, y=SteviloDelovnihUr)) + 
@@ -14,20 +14,33 @@ g2eu <- ggplot(data = A3, aes(x=Leto, y=SteviloDelovnihUr)) +
   theme(panel.background=element_rect(fill="grey"))
 
 g3drzave <- ggplot(data = A1, aes(x=Leto, y=SteviloDelovnihUr)) + 
+  geom_line() + ggtitle("Število delovnih ur po letih") + 
+  theme(panel.background=element_rect(fill="grey"))
+
+
+
+#######  SKUPNO IZRISAVANJE  #########
+graf <- ggplot(data = A1 %>% filter(Drzava %in% c("Slovenia", "European Union - 28 countries")),
+             aes(x=Leto, y=SteviloDelovnihUr, color = Spol, shape = Drzava)) + 
   geom_point() + ggtitle("Število delovnih ur po letih") + 
   theme(panel.background=element_rect(fill="grey"))
 
-#gslo&eu <- merge(g1,g2) + geom_point() + ggtitle("Število delovnih ur po letih") + 
-#  theme(panel.background=element_rect(fill="grey"))
+graf2 <- ggplot(data = A1 %>% filter(Drzava %in% c("Slovenia", "European Union - 28 countries"), Spol %in% "Total"),
+               aes(x=Leto, y=SteviloDelovnihUr, color = Drzava)) + 
+  geom_line() + ggtitle("Število delovnih ur po letih") + 
+  theme(panel.background=element_rect(fill="grey"))
+
+
+
 
 
 #kopirano
-g <- ggplot() + aes(x=Leto, y=SteviloDelovnihUr, color=tip) + facet_grid(tip ~ ., scales="free_y") +
-  geom_line(data= A1 %>% filter(Drzava == "Slovenia")) +
-  geom_line(data=A1 %>% filter(Drzava == "European Union - 28 countries")) + 
-  scale_x_continuous(breaks=seq(2008, 2018, 2)) +
-  scale_y_continuous(labels=comma_format(big.mark="")) +
-  guides(color=FALSE) + ggtitle("Število prostih in zasedenih delovnih mest ")
+#g <- ggplot() + aes(x=Leto, y=SteviloDelovnihUr, color=tip) + facet_grid(tip ~ ., scales="free_y") +
+#  geom_line(data= A1 %>% filter(Drzava == "Slovenia")) +
+#  geom_line(data=A1 %>% filter(Drzava == "European Union - 28 countries")) + 
+#  scale_x_continuous(breaks=seq(2008, 2018, 2)) +
+#  scale_y_continuous(labels=comma_format(big.mark="")) +
+#  guides(color=FALSE) + ggtitle("Število prostih in zasedenih delovnih mest ")
 
 
 
